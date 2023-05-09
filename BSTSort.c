1e3dee56386460afc8cf303b85b373335716bf51
Velo <84917738+Velocities@users.noreply.github.com>
2023-05-09 17:00:44 -0400

    Add inline comments for functions

--- BSTSort.c
+++ BSTSort.c
@@ -7,6 +7,8 @@ static unsigned int index = 0;
 
 static int* arrStorage;
 
+// This function creates a new node with the given value, initializes its occurrence count to 1, and sets its left and right child pointers to NULL.
+// (Our "Node constructor")
 Node* makeNode(int value) {
   Node *result = (Node *) malloc(sizeof(Node));
   result->val = value;
@@ -16,8 +18,15 @@ Node* makeNode(int value) {
   return result;
 }
 
+/*
+This function takes an array of integers arr and its size size as input. It creates a new binary search tree with the 
+first element of the array as the root node, and inserts each subsequent element into the tree using the insert() function. 
+After the tree has been constructed, it populates arrStorage with the sorted values by traversing the tree in order and 
+storing each value the appropriate number of times. Finally, it resets the static index variable to 0.
+*/
 void BSTSort(int arr[], int size) {
   Node *root = makeNode(arr[0]);
+  // Construct the BST
   for (int i = 1; i < size; i += 1) {
     insert(root, arr[i]);
   }
@@ -28,6 +37,12 @@ void BSTSort(int arr[], int size) {
   index = 0;
 }
 
+/*
+This function takes a node pointer n and a value insertionVal as input. It inserts the value into 
+the binary search tree rooted at n, incrementing the occurrence count for the existing node if the 
+value is already in the tree, or creating a new node and inserting it appropriately if the value is 
+not already in the tree.
+*/
 void insert(Node *n, int insertionVal) {
   while (n != NULL) {
     if (insertionVal == n->val) {
@@ -51,6 +66,13 @@ void insert(Node *n, int insertionVal) {
   }
 }
 
+/*
+This is a recursive function that performs an in-order traversal of the binary search tree rooted at n, and 
+inserts each node's value into the global arrStorage array n->occurrences times, where n->occurrences is the 
+number of times the value appears in the input array. The function first recursively traverses the left subtree 
+of n, then inserts n->val n->occurrences times into arrStorage, and finally recursively traverses the right 
+subtree of n. After inserting the value of n and its subtrees, the function also frees the memory allocated for the node.
+*/
 void inOrderInsert(Node *n) {
   if (n->left != NULL) {
     inOrderInsert(n->left);
--- BSTSort.h
+++ BSTSort.h
@@ -1,6 +1,10 @@
+/*
+Here we define a struct Node which represents a node in a binary search tree. 
+Each node stores an integer value, the number of times the value occurs in the tree, and pointers to its left and right children.
+*/
 typedef struct Node {
   int val;
-  unsigned int occurrences;
+  unsigned int occurrences; // The reason we need this variable is due to the nature of a BST having no duplicate nodes
   struct Node *left;
   struct Node *right;
 } Node;
